e285d678366472d457fba4038b155e04fd5b50e7
Jamie Wilkinson <jamie@jamiedubs.com>
2019-10-14 15:37:41 -0400

    Make run.sh work

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+playlist=$1
+
+if [ -z $playlist ]; then
+  echo "$0: specify playlist as first and only argument"
+  exit 1
+fi
+
+echo "Downloading $playlist ..."
+output=$(spotdl --playlist $playlist)
+echo $output
+
+# FIXME real sloppy regexin here
+# should just use awk or sed instead?
+textfile=$(echo "$output" | egrep -o "to (.+)\.txt$" | cut -d' ' -f2)
+echo "Playlist file => $textfile"
+
+outdir="$HOME/Music/spotify-rip"
+echo "output directory: $outdir"
+
+spotdl --trim-silence -f "$outdir" --overwrite skip --list "$textfile"
+
+# TODO import straight into Serato like a boss
